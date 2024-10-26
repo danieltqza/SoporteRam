ae525ab784d817c83ae139b7b56ed6d5ec702a71
danieltqza <162772274+danieltqza@users.noreply.github.com>
2024-10-26 10:53:11 -0500

    Add files via upload

---
+++ _astro/hoisted.CthQMFK-.js
@@ -0,0 +1,1 @@
+const t=document.getElementById("btnChange"),n=document.body;function o(){const e=parseInt(document.getElementById("fontSize").value);n.style.fontSize=e+"px"}t?.addEventListener("click",o);
