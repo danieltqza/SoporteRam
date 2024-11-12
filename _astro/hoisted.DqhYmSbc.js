3f9a3cb8ae75fe3d40f6e24211090ce4d7b273b4
danieltqza <162772274+danieltqza@users.noreply.github.com>
2024-11-12 04:40:43 -0500

    Añadiendo mejoras y haciendo el tema cache y ROM
    Se arregla el logotipo que no se mostraba

---
+++ _astro/hoisted.DqhYmSbc.js
@@ -0,0 +1,1 @@
+import"./hoisted.CthQMFK-.js";document.addEventListener("DOMContentLoaded",()=>{const e=document.getElementById("fullscreenButton");function t(){document.getElementById("pdfViewer")?.requestFullscreen().catch(n=>{console.error(`Error al activar pantalla completa: ${n.message}`)})}e?.addEventListener("click",t)});
